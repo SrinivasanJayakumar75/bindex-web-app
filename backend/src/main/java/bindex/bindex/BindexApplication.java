a4dfd3bb969f715cf0ba11f246b078ccb219175f
SrinivasanJayakumar75 <sjayakumar862412@gmail.com>
2026-02-16 18:36:05 +0530

    chore: set up

---
+++ backend/src/main/java/bindex/bindex/BindexApplication.java
@@ -0,0 +1,13 @@
+package bindex.bindex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BindexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BindexApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/bindex/bindex/BindexApplicationTests.java
@@ -0,0 +1,13 @@
+package bindex.bindex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BindexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
